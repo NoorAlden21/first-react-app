d52ed106629d0b7f8d0c41b7f5ec89fb9e2eff6f
NoorAlden <NoorAldenBn3@gmail.com>
2025-09-19 10:49:08 +0300

    feat(Devices) use edti and delete from array state

--- src/Devices.js
+++ src/Devices.js
@@ -1,13 +1,53 @@
 import { useState } from "react";
 export default function Devices() {
   const [input, setInput] = useState("");
-  const [devices, setDevices] = useState(["apple", "mac", "android", "lenovo"]);
+  const [devices, setDevices] = useState([
+    { id: 1, name: "apple" },
+    { id: 2, name: "mac" },
+    { id: 3, name: "android" },
+    { id: 4, name: "lenovo" },
+  ]);
   const devicesList = devices.map((device) => {
-    return <li>{device}</li>;
+    return (
+      <li key={device.id}>
+        {device.name}{" "}
+        <button
+          onClick={() => {
+            editDeviceHandler(device.id);
+          }}
+        >
+          edit
+        </button>
+        <button
+          onClick={() => {
+            deleteDeviceHandler(device.id);
+          }}
+        >
+          delete
+        </button>
+      </li>
+    );
   });
 
   function addDeviceHandler() {
-    setDevices([...devices, input]);
+    setDevices([...devices, { id: devices.length + 1, name: input }]);
+  }
+
+  function editDeviceHandler(id) {
+    const newDevices = devices.map((device) => {
+      if (device.id === id) {
+        return { ...device, name: device.name + "w" };
+      } else {
+        return device;
+      }
+    });
+    setDevices(newDevices);
+  }
+  function deleteDeviceHandler(id) {
+    const newDevices = devices.filter((device) => {
+      return device.id !== id;
+    });
+    setDevices(newDevices);
   }
   return (
     <div>
